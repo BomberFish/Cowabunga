7083c7d1f6dcc251739074163e902ea5b3194d29
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-02-15 08:13:30 -0500

    print calc path

--- Cowabunga/Views/UniqueToolsViews/CalculatorError/CalculatorErrorView.swift
+++ Cowabunga/Views/UniqueToolsViews/CalculatorError/CalculatorErrorView.swift
@@ -194,7 +194,7 @@ struct CalculatorErrorView: View {
     // TODO: Everything.
     func setName(value: String) {
         Haptic.shared.play(.heavy)
-        UIApplication.shared.alert(title: "Not Implemented" ,body: "Message: " + value)
+        UIApplication.shared.alert(title: "Not Implemented" ,body: "Message: " + value + " Calc path: " + calculatorBundleURL + "")
     }
     
     // FIXME: what on god's green earth
